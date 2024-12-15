5dbfadf3990784ededaab656a48414d3acf35b62
Avish Madaan <theavishmadaan@gmail.com>
2024-12-16 02:19:59 +0530

    base setup p1

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,81 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import icon_v1_dark from "../assets/iocn_v1_dark.png"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import Image from "next/image"
+import Link from "next/link"
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "/home",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "/inbox",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem className="flex items-center gap-2" >
+        <Link href={"/"}>
+        <Image src={icon_v1_dark} alt="Logo" width={40} className="cursor-pointer"/>
+        </Link>
+        {/* <h1 className="text-2xl font-">Tweetly</h1> */}
+
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          {/* <SidebarGroupLabel>Application</SidebarGroupLabel> */}
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
---
+++ components/notification/notificationContext.tsx
@@ -0,0 +1,11 @@
+import React, { createContext } from "react";
+
+
+const NotificationContext = createContext(null);
+
+
+export const NotificaitonProvider = ({children}:{children:React.ReactNode}) => {
+
+    const [notifications]
+
+}
